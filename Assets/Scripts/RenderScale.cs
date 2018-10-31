c18d1b43bc708a6059315c5f7f2550eacf28ced3
bradengibson <32143413+bradengibson@users.noreply.github.com>
2018-10-31 15:50:42 -0400

    Added a script to modify the render scale
    Kept at the default of 1.0, but can increase if performance is not an issue.

---
+++ Assets/Scripts/RenderScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class RenderScale : MonoBehaviour {
+	
+	[SerializeField] private float m_RenderScale = 1f;   // The render scale. Higher numbers = better quality, but trade performance.
+
+	// Use this for initialization
+	void Start () {
+		VRSettings.renderScale = m_RenderScale;
+	}
+}
