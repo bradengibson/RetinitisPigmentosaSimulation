879617711fa3348096f786327be3ba9a666e4838
bradengibson <32143413+bradengibson@users.noreply.github.com>
2018-11-12 13:17:43 -0500

    Tunnel Vision is repositioning according to gaze
    Need to rotate and resize

---
+++ Assets/Scripts/TunnelVisionPositioning.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelVisionPositioning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		FoveInterface.EyeRays eyes = FoveInterface.GetEyeRays();
+
+		switch (FoveInterface.CheckEyesClosed())
+		{
+		case Fove.EFVR_Eye.Neither:
+
+			transform.position = eyes.left.GetPoint(3.0f) + ((eyes.right.GetPoint(3.0f) - eyes.left.GetPoint(3.0f)) / 2);
+			break;
+
+		case Fove.EFVR_Eye.Left:
+
+			transform.position = eyes.right.GetPoint(3.0f);
+			break;
+
+		case Fove.EFVR_Eye.Right:  
+
+			transform.position = eyes.left.GetPoint(3.0f);
+			break;
+
+		}
+	}
+}
